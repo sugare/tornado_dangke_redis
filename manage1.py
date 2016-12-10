9cefc54f4bacbc9cb68eb14de3d35bbfc5976569
song <30733705@qq.com>
2016-12-10 15:40:10 +0800

    the first

---
+++ manage1.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 16-12-10 上午9:03
+# @Author  : sugare
+# @Site    : 
+# @File    : manage1.py
+# @Software: PyCharm
+import random
+import os.path
+import tornado.web
+import tornado.httpserver
+import tornado.gen
+import os
+import redis
+from updata1 import downloadMask
+
+from tornado.options import define, options
+define("port", default=8888, help="run on the given port", type=int)
+define("redis_host", default="127.0.0.1", help="redis host")
+define("redis_port", default=6379, help="redis port")
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/?', LoginHandler),
+            (r'/exam/?', ExamHandler),
+            (r'/investigation/?', InvestigationHandler),
+            (r'/score/?(.*)', ScoreHandler),
+            (r'/logout/?', LogoutHandler),
+        ]
+        settings = dict(
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            template_path=os.path.join(os.path.dirname(__file__), 'template'),
+            xsrf_cookies=True,
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            login_url="/login",
+            debug=True,
+        )
+        super(Application, self).__init__(handlers, **settings)
+        # Have one global connection to the blog DB across all handlers
+
+        self.db = redis.Redis(
+            host=options.redis_host, port=options.redis_port, db=0
+        )
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property       # 将db方法变为属性
+    def db(self):
+        return self.application.db
+
+    def get_current_user(self):
+        user_id = self.get_secure_cookie("username")
+        if not user_id:
+            return None
+        return user_id
+
+
+class LoginHandler(BaseHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        self.render('login.html')
+
+    @tornado.web.asynchronous
+    @tornado.gen.coroutine
+    def post(self):
+        username = self.get_argument('username', '')
+        password = self.get_argument('password', '')
+        if len(self.db.hgetall(username)) == 5 and self.db.hgetall(password) == self.db.hgetall(username):
+            self.set_secure_cookie("username", username)
+            self.redirect('/exam')
+        else:
+            if len(self.db.hgetall(username)) > 5:
+                self.write(u'您已经提交试卷！')
+                self.finish()
+            else:
+                self.redirect('/')
+
+
+class ExamHandler(BaseHandler):
+    def quesNum(self,ty):
+        if ty == 's' or ty == 'm':
+            L = [ str(x) for x in range(1,61) ]
+            Q = random.sample(L, 20)
+        elif ty == 'j':
+            L = [ str(x) for x in range(1,31) ]
+            Q = random.sample(L,10)
+        elif ty == 'i':
+            L = [ str(x) for x in range(1,6) ]
+            Q = random.sample(L,5)
+        return map(lambda num: ty+num, Q)
+
+    # @tornado.web.authenticated
+    # @tornado.web.asynchronous
+    # @tornado.gen.coroutine
+    def get(self):
+
+        s = {'rubbish':'s'}
+        m = {'rubbish':'m'}
+        j = {'rubbish':'j'}
+        for i in (s, m, j):
+            for k in self.quesNum(i.pop('rubbish')):
+                i[k] = self.db.hgetall(k)
+
+        self.render('exam1.html', user=self.current_user, s_ques=s, m_ques=m, j_ques=j)
+
+    # @tornado.web.authenticated
+    # @tornado.web.asynchronous
+    # @tornado.gen.coroutine
+    def post(self):
+        s_mask = 0
+        m_mask = 0
+        j_mask = 0
+        total = 0
+
+        self.request.arguments.pop('_xsrf')
+        for i in self.request.arguments:
+            user_rec = ''.join(self.request.arguments[i])
+            answer = self.db.hget(i, 'AN')
+            self.db.hset(self.get_secure_cookie('username'), i, user_rec)
+            if user_rec == answer:
+                if 's' in i:
+                    s_mask += 2
+                elif 'm' in i:
+                    m_mask += 2
+                else:
+                    j_mask += 2
+        total = s_mask + m_mask + j_mask
+        self.db.hset(self.get_secure_cookie('username'), 'single', s_mask)
+        self.db.hset(self.get_secure_cookie('username'), 'multi', m_mask)
+        self.db.hset(self.get_secure_cookie('username'), 'judge', j_mask)
+        self.db.hset(self.get_secure_cookie('username'), 'total', total)
+        self.redirect('/investigation')
+
+class InvestigationHandler(ExamHandler):
+    # @tornado.gen.coroutine
+    def get(self):
+        i_ques = {}
+        for i in self.quesNum('i'):
+            i_ques[i] = self.db.hgetall(i)
+        self.render('invest1.html', i_ques=i_ques)
+
+    # @tornado.gen.coroutine
+    def post(self):
+        self.request.arguments.pop('_xsrf')
+        for i in ('i1', 'i2' ,'i3' ,'i4', 'i5'):
+            v = self.get_argument(i,'A')
+            self.db.hset(i, v, int(self.db.hget(i, v)) + 1)
+        self.redirect('/logout')
+
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("username")
+        self.write(u'交卷成功！')
+        self.redirect(self.get_argument("next", "/"), )
+
+
+class ScoreHandler(ExamHandler):
+    def get(self, slug=''):
+        downloadMask()
+        if slug:
+            s = self.db.hgetall(slug)
+            s.pop('user')
+            s.pop('single')
+            s.pop('multi')
+            s.pop('judge')
+            s.pop('total')
+            for i in s:
+                s[i] = [s[i], self.db.hget(i, 'AN')]
+            self.render('score_detail1.html', s=s)
+        else:
+
+            s = {}
+            for i in self.db.keys(pattern='2*'):
+                s[i] = self.db.hgetall(i)
+            self.render('score1.html', s=s)
+
+
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.current().start()
+
+
+if __name__ == "__main__":
+    main()
---
+++ updata1.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 16-12-1 下午7:07
+# @Author  : sugare
+# @Site    : 
+# @File    : updata1.py
+# @Software: PyCharm
+import os
+import redis
+import xlrd
+import xlwt
+
+r = redis.Redis(host='127.0.0.1', port=6379, db=0)
+
+def upUsers():         # 上传用户
+    data = xlrd.open_workbook(r'static/data/users.xls')
+    table = data.sheet_by_index(0)
+    nrows = table.nrows
+    for i in (range(1, nrows + 1)):
+        r.hset(int(table.row_values(i - 1)[0]), 'user', table.row_values(i - 1)[2])
+        r.hset(int(table.row_values(i - 1)[0]), 'single', 0)
+        r.hset(int(table.row_values(i - 1)[0]), 'multi', 0)
+        r.hset(int(table.row_values(i - 1)[0]), 'judge', 0)
+        r.hset(int(table.row_values(i - 1)[0]), 'total', 0)
+
+
+def upQuestionChoice():
+    data = xlrd.open_workbook(r'static/data/question.xlsx')
+    table = data.sheet_by_index(0)
+    # nrows = table.nrows
+    for i in range(1,151):
+        if i < 61:
+            r.hset('s'+ str(i), 'q', table.row_values(i-1)[0])
+            r.hset('s'+ str(i), 'A', table.row_values(i-1)[1])
+            r.hset('s'+ str(i), 'B', table.row_values(i-1)[2])
+            r.hset('s'+ str(i), 'C', table.row_values(i-1)[3])
+            r.hset('s'+ str(i), 'D', table.row_values(i-1)[4])
+            r.hset('s'+ str(i), 'AN', table.row_values(i-1)[5])
+
+        elif i > 120:
+            r.hset('j' + str(i - 120), 'q', table.row_values(i - 1)[0])
+            r.hset('j' + str(i - 120), 'AN', table.row_values(i - 1)[5])
+
+        else:
+            r.hset('m'+ str(i-60), 'q', table.row_values(i-1)[0])
+            r.hset('m'+ str(i-60), 'A', table.row_values(i-1)[1])
+            r.hset('m'+ str(i-60), 'B', table.row_values(i-1)[2])
+            r.hset('m'+ str(i-60), 'C', table.row_values(i-1)[3])
+            r.hset('m'+ str(i-60), 'D', table.row_values(i-1)[4])
+            r.hset('m'+ str(i-60), 'AN', table.row_values(i-1)[5])
+
+
+def upSurvey():
+    data = xlrd.open_workbook(r'static/data/fujia.xls')
+    table = data.sheet_by_index(0)
+    for i in range(6, 11):
+        r.hset('i' + str(i-5), 'q', table.row_values(i - 1)[1])
+        r.hset('i' + str(i-5), 'A', 0)
+        r.hset('i' + str(i-5), 'B', 0)
+        r.hset('i' + str(i-5), 'C', 0)
+        r.hset('i' + str(i-5), 'D', 0)
+        r.hset('i' + str(i-5), 'E', 0)
+
+
+def downloadMask():     # 下载成绩单
+    workbook = xlwt.Workbook()
+    sheet1 = workbook.add_sheet('results', cell_overwrite_ok=True)
+    sheet2 = workbook.add_sheet('investigation', cell_overwrite_ok=True)
+    sheet2.write(0, 0, u'问题\选项')
+    sheet2.write(0, 1, u'A.非常满意')
+    sheet2.write(0, 2, u'B.满意')
+    sheet2.write(0, 3, u'C.比较满意')
+    sheet2.write(0, 4, u'D.一般')
+    sheet2.write(0, 5, u'E.不满意')
+
+    for i in range(1, 6):
+        a = r.hgetall('i'+str(i))
+        sheet2.write(i, 0, a['q'].decode('utf8'))
+        sheet2.write(i, 1, a['A'])
+        sheet2.write(i, 2, a['B'])
+        sheet2.write(i, 3, a['C'])
+        sheet2.write(i, 4, a['D'])
+        sheet2.write(i, 5, a['E'])
+
+    sheet1.write(0, 0, u'考号')
+    sheet1.write(0, 1, u'姓名')
+    sheet1.write(0, 2, u'单选')
+    sheet1.write(0, 3, u'多选')
+    sheet1.write(0, 4, u'判断')
+    sheet1.write(0, 5, u'总分')
+    sc = r.keys(pattern='2*')
+    n = 0
+
+    for i in sc:
+        n += 1
+        sheet1.write(n, 0, i)
+        sheet1.write(n, 1, r.hget(i, 'user').decode('utf8'))
+        sheet1.write(n, 2, r.hget(i, 'single'))
+        sheet1.write(n, 3, r.hget(i, 'multi'))
+        sheet1.write(n, 4, r.hget(i, 'judge'))
+        sheet1.write(n, 5, r.hget(i, 'total'))
+    workbook.save(r'%s/static/data/mask1.xls' % (os.path.dirname(__file__)))
+
+
+if __name__ == '__main__':
+    #upUsers()
+    #upSurvey()
+    #upQuestionChoice()
+    downloadMask()
